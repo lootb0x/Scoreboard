26dd4d4b6e9ab63cb6026a8555f2968c01a5a54f
lootb0x <justinlin8031@gmail.com>
2024-10-23 13:33:39 +0000

    fin pls

--- Scoreboard.java
+++ Scoreboard.java
@@ -4,31 +4,31 @@ public class Scoreboard
     private String teamTwo;
     private int teamOneScore;
     private int teamTwoScore;
-    private String activeTeam;
-    teamOne = one;
-    teamTwo = two;
-    activeTeam = TeamOne;
-    public Scoreboard(String one, String two)
+    private String active;
+
+        public Scoreboard(String one, String two)
     {
         teamOne = one;
         teamTwo = two;
+        active = teamOne;
+
     }
     public String getScore()
         {
-            return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+            return teamOneScore + "-" + teamTwoScore + "-" + active;
         }
     
     public void recordPlay(int score)
     {  
-        if(teamOne.equals(activeTeam))  
+        if(teamOne.equals(active))  
         {teamOneScore += score;}
-        if(teamTwo.equals(activeTeam))  
+        if(teamTwo.equals(active))  
         {teamTwoScore += score;}
         
         if(score == 0)
         {
-            if(activeTeam.equals(teamOne)) activeTeam = teamTwo;
-            else if(activeTeam.equals(teamTwo)) activeTeam = teamOne;
+            if(active.equals(teamOne)) active = teamTwo;
+            else if(active.equals(teamTwo)) active = teamOne;
         }
 
     }
